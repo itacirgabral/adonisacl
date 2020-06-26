91473dbd44a19ef4065ebb39900343f71a92e955
Itacir Gabral <itacirgabral@gmail.com>
2020-06-26 15:01:29 -0400

    apiOnly

--- start/routes.js
+++ start/routes.js
@@ -18,4 +18,4 @@ const Route = use('Route')
 
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
-Route.resource('projects', 'ProjectController').middleware('auth')
+Route.resource('projects', 'ProjectController').apiOnly().middleware('auth')
